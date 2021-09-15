aa5a5aa40259017d6466e25cbdb960d6d0cf3ef3
Mauricio Freitas <contato@mauriciofreitas.eng.br>
2021-09-14 23:44:17 -0300

    Refactor Main Application (#3)

--- snake/main.py
+++ snake/main.py
@@ -1,70 +1,157 @@
-"""Main."""
+"""Main Application."""
+from functools import cached_property
+
 import pygame
-from pygame.rect import Rect
+from pygame.event import Event
+from pygame.font import SysFont, get_default_font
+from pygame.surface import Surface
+from pygame.time import Clock
 
 from snake.snake import Snake
 from snake.utils import time_ms
 
 
+class QuitApplication(Exception):
+    """Quit the application if raised inside the Main Loop."""
+
+
 class MainApp:
     """Main application."""
 
+    #: Screen/Window parameters.
     CAPTION = "Snake v0.1"
 
     WIDTH = 800
     HEIGHT = 800
+    SIZE = (WIDTH, HEIGHT)
 
     BG_COLOR = (0x00, 0x00, 0x00)
 
+    #: FPS meter parameters.
+    FPS_SIZE = 25
+    FPS_COLOR = (0xFF, 0x00, 0x00)
+
+    #: Grid parameters.
+    GRID_ALPHA = 50
+    GRID_COLOR = (0xFF, 0xFF, 0xFF)
     GRID_SIZE = 20
-    GRID_COLOR = (0xFF, 0x00, 0x00)
     GRID_WIDTH = 1
 
-    TICK_STEP = 20.0
+    #: Difference in time between ticks.
+    TICK_STEP = 20.0  # ms
 
-    def __init__(self):
-        self._running = True
-        self._next_tick: float = time_ms()
+    #: Max number of rendered frames that can be skipped. This is mostly
+    #  relevant on slower machines, in case the time it takes to update the
+    #  game state is greater than `TICK_STEP`.
 
+    MAX_FRAMESKIP = 10
+
+    def __init__(self, grid: bool, show_fps: bool):
+        """Main Application.
+
+        :param bp_name: Name of the Blueprint to be loaded.
+        :param grid: If `True`, draw a grid on top of the screen.
+        :param show_fps: If `True`, render the FPS on screen.
+        """
+        self._grid = grid
+        self._show_fps = show_fps
+
+        # Init PyGame
         pygame.init()
-
-        # TODO: Add Logo
         pygame.display.set_caption(self.CAPTION)
 
-        self._screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        # Application Variables
+        self._next_tick = time_ms()
+        self._render_clock = Clock()
+        self._running = True
+
+        # Game Elements
+        self._screen = pygame.display.set_mode(size=(self.WIDTH, self.HEIGHT))
+
+        self._fps_font = SysFont(get_default_font(), self.FPS_SIZE)
+
         self._snake = Snake(grid_size=self.GRID_SIZE)
 
-    def draw_grid(self) -> None:
-        """Draw grid."""
+    @property
+    def _fps_surface(self) -> Surface:
+        """FPS Meter Surface."""
+        msg = f"FPS: {self._render_clock.get_fps()}"
+        return self._fps_font.render(msg, True, self.FPS_COLOR)
+
+    @cached_property
+    def _grid_surface(self) -> Surface:
+        """A surface representing the Grid."""
+        surface = Surface(size=self.SIZE, flags=pygame.SRCALPHA)
+        surface.set_alpha(self.GRID_ALPHA)
         for x in range(0, self.WIDTH, self.GRID_SIZE):
             for y in range(0, self.HEIGHT, self.GRID_SIZE):
                 pygame.draw.rect(
-                    surface=self._screen,
+                    surface=surface,
                     color=self.GRID_COLOR,
-                    rect=Rect(x, y, self.GRID_SIZE, self.GRID_SIZE),
+                    rect=pygame.Rect((x, y), (self.GRID_SIZE, self.GRID_SIZE)),
                     width=self.GRID_WIDTH,
                 )
 
-    def handle_events(self) -> None:
-        """Handle events."""
+        return surface
+
+    def _handle_quit(self, event: Event) -> None:
+        """Handle the Quit events.
+
+        :param event: PyGame Event object.
+        """
+        if event.type == pygame.QUIT:
+            raise QuitApplication
+        elif event.type == pygame.KEYUP and event.key == pygame.K_q:
+            raise QuitApplication
+
+    def _update_game(self) -> None:
+        """Update Game State.
+
+        :param tick: Current tick in ms.
+        """
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self._running = False
+            self._handle_quit(event=event)
+            self._snake.handle_event(event=event)
 
-            self._snake.handle_input(event=event)
+    def _calc_interpolation(self) -> float:
+        """Calculate the Interpolation between game ticks."""
+        next_prediction = time_ms() + self.TICK_STEP - self._next_tick
+        interp = next_prediction / self.TICK_STEP
+        return max(min(interp, 1.0), 0.0)  # Clip between 0 and 1
 
-    def render_screen(self) -> None:
-        """Render the screen."""
+    def _render_graphics(self, interp: float) -> None:
+        """Render the frame and display it in the screen.
+
+        :param interp: To allow smoother movement on screen, interpolation is
+          used when rendering the screen between game state updates,
+        """
+        layers = [(self._snake.surface, self._snake.render_pos)]
+        if self._grid:
+            layers.append((self._grid_surface, (0, 0)))
+
+        if self._show_fps:
+            layers.append((self._fps_surface, (0, 0)))
+
         self._screen.fill(color=self.BG_COLOR)
-        self.draw_grid()
-        self._snake.draw(surface=self._screen)
+        self._screen.blits(layers)
         pygame.display.flip()
+        self._render_clock.tick()
 
-    def execute(self):
-        """Application main loop."""
-        while self._running:
-            if time_ms() > self._next_tick:
-                self.handle_events()
-                self._next_tick += self.TICK_STEP
+    def _main_loop(self) -> None:
+        """Main Application Loop."""
+        loops = 0
+        while time_ms() > self._next_tick and loops < self.MAX_FRAMESKIP:
+            self._update_game()
+            self._next_tick += self.TICK_STEP
+            loops += 1
 
-            self.render_screen()
+        interpolation = self._calc_interpolation()
+        self._render_graphics(interp=interpolation)
+
+    def run(self) -> None:
+        """Run the application."""
+        while True:
+            try:
+                self._main_loop()
+            except QuitApplication:
+                break
--- snake/snake.py
+++ snake/snake.py
@@ -1,10 +1,9 @@
-"""Represent the main protagonist."""
+"""Represent the Main Protagonist."""
 from typing import Tuple
 
 import pygame
 from pygame import Surface
 from pygame.event import Event
-from pygame.rect import Rect
 
 
 class Snake:
@@ -13,13 +12,13 @@ class Snake:
     HEAD_COLOR = (0x00, 0xFF, 0x00)
 
     def __init__(self, grid_size: int):
+        """Create new Snake, controlled by the player.
+
+        :param grid_size: Size of the Grid in Pixels.
+        """
         self._x: int = 0
         self._y: int = 0
         self._grid_size = grid_size
-
-    @property
-    def pos(self) -> Tuple[int, int]:
-        return self._x, self._y
 
     def move_x(self, step: int) -> None:
         self._x += step
@@ -27,20 +26,22 @@ class Snake:
     def move_y(self, step: int) -> None:
         self._y += step
 
-    def draw(self, surface: Surface):
-        """Draw the snake on the surface."""
-        pygame.draw.rect(
-            surface=surface,
-            color=self.HEAD_COLOR,
-            rect=Rect(
-                self._x * self._grid_size,
-                self._y * self._grid_size,
-                self._grid_size,
-                self._grid_size,
-            ),
-        )
+    @property
+    def pos(self) -> Tuple[int, int]:
+        return self._x, self._y
 
-    def handle_input(self, event: Event) -> None:
+    @property
+    def render_pos(self) -> Tuple[int, int]:
+        return self._x * self._grid_size, self._y * self._grid_size
+
+    @property
+    def surface(self) -> Surface:
+        """Draw the snake on the surface."""
+        surface = Surface(size=(self._grid_size, self._grid_size))
+        surface.fill(color=self.HEAD_COLOR)
+        return surface
+
+    def handle_event(self, event: Event) -> None:
         """Handle events."""
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
